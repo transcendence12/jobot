d6c7b701a83cfd2723e2028d51dec7300cfc5fcc
transcendence12 <gosiek.krawczuk@gmail.com>
2025-01-26 14:23:12 +0100

    feat: create scrapper

--- src/bot/scrapper/scrapper.ts
+++ src/bot/scrapper/scrapper.ts
@@ -1,3 +1,56 @@
+import puppeteer, { Browser, Page } from "puppeteer";
+import { ScrapperOptions } from "./types";
 export class Scrapper {
-    // Scrapper should be a set of methods that you could use for navigating through scrapped website.
+  // Scrapper should be a set of methods that you could use for navigating through scrapped website.
+  browser: Browser | null = null;
+  page: Page | null = null;
+  options: ScrapperOptions | null = null;
+
+  async init() {
+    this.browser = await puppeteer.launch({ headless: false, slowMo: 250 });
+    this.page = await this.browser.newPage();
+  }
+
+  async navigateTo(url: string) {
+    if (!this.page) {
+      throw new Error("Page not initialized");
+    }
+    await this.page.goto(url);
+  }
+
+  /**
+   * Extracts content from a child element within a parent element using a CSS selector
+   * @param element - Parent element to search within
+   * @param selector - CSS selector to find the child element
+   * @param attribute - Optional attribute to extract from child element. If not provided, returns text content
+   * @returns Extracted attribute value or text content as string, empty string if element/attribute not found
+   */
+  async extractFromElement(
+    element: any | null,
+    selector: string,
+    attribute?: string
+  ) {
+    if (!this.page || !element) return "";
+    const childElement = await element.$(selector);
+    if (!childElement) return "";
+
+    if (attribute) {
+      return await this.page.evaluate(
+        (el, attribute) => el.getAttribute(attribute),
+        childElement,
+        attribute
+      );
+    } else {
+      return await this.page.evaluate(
+        (el) => el.textContent.trim(),
+        childElement
+      );
+    }
+  }
+
+  async close() {
+    if (this.browser) {
+      await this.browser.close();
+    }
+  }
 }
--- src/scripts/findOffers.ts
+++ src/scripts/findOffers.ts
@@ -1,9 +1,21 @@
-const findOffers = () => {
-    console.log('Scrapping...')
+import { Scrapper } from "../bot/scrapper/scrapper";
+import { ScrapperOptions } from "../bot/scrapper/types";
 
-    setTimeout(() => {
-        console.log('10 offers found.')
-    }, 3000)
-}
+const findOffers = async (searchValue: string, maxRecords: number = 10) => {
+  console.log("Scrapping...");
+  const options: ScrapperOptions = {
+    searchValue,
+    maxRecords,
+  };
+  const scrapper = new Scrapper();
+  scrapper.options = options;
+  await scrapper.init();
+  await scrapper.navigateTo(`https://bulldogjob.pl/companies/jobs/s/role,${searchValue}`);
 
-findOffers()
+
+  setTimeout(() => {
+    console.log(`${options.maxRecords} offers found.`);
+  }, 3000);
+};
+
+findOffers("backend", 10);
