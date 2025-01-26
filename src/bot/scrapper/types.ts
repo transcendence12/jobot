c710b0c9475ad708713377bbcdb4cdfb6b3294d3
transcendence12 <gosiek.krawczuk@gmail.com>
2025-01-26 15:20:00 +0100

    Revert "feat: create scrapper"

--- src/bot/scrapper/types.ts
+++
@@ -1,4 +0,0 @@
-export interface ScrapperOptions {
-  searchValue: string;
-  maxRecords: number;
-}
--- src/scripts/findOffers.ts
+++ src/scripts/findOffers.ts
@@ -1,21 +1,9 @@
-import { Scrapper } from "../bot/scrapper/scrapper";
-import { ScrapperOptions } from "../bot/scrapper/types";
+const findOffers = () => {
+    console.log('Scrapping...')
 
-const findOffers = async (searchValue: string, maxRecords: number = 10) => {
-  console.log("Scrapping...");
-  const options: ScrapperOptions = {
-    searchValue,
-    maxRecords,
-  };
-  const scrapper = new Scrapper();
-  scrapper.options = options;
-  await scrapper.init();
-  await scrapper.navigateTo(`https://bulldogjob.pl/companies/jobs/s/role,${searchValue}`);
+    setTimeout(() => {
+        console.log('10 offers found.')
+    }, 3000)
+}
 
-  const offers = await scrapper.scrapeJobOffers(maxRecords);
-  console.log(`${offers.length} offers found:`, offers);
-
-  await scrapper.close();
-};
-
-findOffers("backend", 10);
+findOffers()
